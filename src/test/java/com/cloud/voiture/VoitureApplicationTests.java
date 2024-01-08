e9f4ec60d42ca28f110af00429ec78f10c188371
Nante <nantemino15@gmail.com>
2024-01-08 09:47:27 +0300

    initial commit

---
+++ src/test/java/com/cloud/voiture/VoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.voiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
