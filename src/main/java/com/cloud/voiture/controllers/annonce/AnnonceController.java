fabf98884506b122bbefad9b451b1f241ef20c5f
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2024-01-08 19:37:18 +0100

    feat [ANNONCE VALIDATION] annonces non valides

--- src/main/java/com/cloud/voiture/controllers/annonce/AnnonceController.java
+++ src/main/java/com/cloud/voiture/controllers/annonce/AnnonceController.java
@@ -1,13 +1,30 @@
 package com.cloud.voiture.controllers.annonce;
 
+import com.cloud.voiture.crud.controller.GenericController;
+import com.cloud.voiture.models.annonce.Annonce;
+import com.cloud.voiture.services.annonce.AnnonceService;
+import com.cloud.voiture.types.response.Response;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cloud.voiture.crud.controller.GenericController;
-import com.cloud.voiture.models.annonce.Annonce;
-import com.cloud.voiture.models.voiture.Categorie;
 @RestController
-@RequestMapping("/api/annonces")
-public class AnnonceController extends GenericController<Annonce>{
-    
+@RequestMapping("/annonces")
+public class AnnonceController extends GenericController<Annonce> {
+
+  @Autowired
+  AnnonceService annonceService;
+
+  @GetMapping("/nonValide")
+  public ResponseEntity<Response> findNonValide() {
+    try {
+      List<Annonce> results = annonceService.getAllNonValide();
+      return ResponseEntity.ok(new Response(results, ""));
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body(new Response(e.getMessage()));
+    }
+  }
 }
--- src/main/java/com/cloud/voiture/repositories/annonce/AnnonceRepository.java
+++ src/main/java/com/cloud/voiture/repositories/annonce/AnnonceRepository.java
@@ -2,5 +2,11 @@ package com.cloud.voiture.repositories.annonce;
 
 import com.cloud.voiture.crud.repository.GenericRepository;
 import com.cloud.voiture.models.annonce.Annonce;
+import java.util.List;
 
-public interface AnnonceRepository extends GenericRepository<Annonce> {}
+import org.springframework.data.jpa.repository.Query;
+
+public interface AnnonceRepository extends GenericRepository<Annonce> {
+    @Query(value ="select * from v_annonce_non_valide", nativeQuery = true)
+    List<Annonce> getAllNonValide();
+}
