d1a4a7b985206507839c89140d6876430774e868
Mialy05 <mialisoamurielle@gmail.com>
2024-01-12 18:18:58 +0300

    feat [CONTRAINTE BDD] gestion erreur  lors des contraintes psql

--- src/main/java/com/cloud/voiture/crud/controller/GenericController.java
+++ src/main/java/com/cloud/voiture/crud/controller/GenericController.java
@@ -3,11 +3,22 @@ package com.cloud.voiture.crud.controller;
 import com.cloud.voiture.crud.model.GenericModel;
 import com.cloud.voiture.crud.pagination.Paginated;
 import com.cloud.voiture.crud.service.GenericService;
+import com.cloud.voiture.models.voiture.Etat;
+import com.cloud.voiture.services.utilities.Utilities;
 import com.cloud.voiture.types.response.Response;
+
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
+
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,76 +27,116 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class GenericController<T extends GenericModel> {
+    private final Class<T> type;
+    @Autowired
+    GenericService<T> service;
 
-  @Autowired
-  GenericService<T> service;
+    HashMap<String, Object> res;
+    HashMap<String, Object> err;
 
-  HashMap<String, Object> res;
-  HashMap<String, Object> err;
-
-  public GenericController() {
-    res = new HashMap<>();
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<?> find(@PathVariable(name = "id") int id) {
-    try {
-      T results = service.find(id);
-      return ResponseEntity.ok(new Response(results, ""));
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(new Response(e.getMessage()));
+    public GenericController() {
+        res = new HashMap<>();
+        type = (Class<T>) ResolvableType.forClass(getClass()).getSuperType().getGeneric(0).resolve();
     }
-  }
 
-  @PostMapping
-  public ResponseEntity<?> save(@RequestBody T model) {
-    try {
-      T results = service.save(model);
-      return ResponseEntity.ok(new Response(results, "Inserer avec succes"));
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(new Response(e.getMessage()));
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable(name = "id") int id) {
+        try {
+            T results = service.find(id);
+            return ResponseEntity.ok(new Response(results, ""));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new Response(e.getMessage()));
+        }
     }
-  }
 
-  @GetMapping
-  public ResponseEntity<?> findAll(
-    @RequestParam(required =false, defaultValue = "0") int page,
-    @RequestParam(required = false, defaultValue = "0") int pageSize
-  ) {
-    try {
-      if (page == 0 && pageSize == 0) {
-        List<T> results = service.findAll();
-        return ResponseEntity.ok(new Response(results, ""));
-      }
-      Paginated<T> result = service.findAll(page, pageSize);
-      return ResponseEntity.ok(new Response(result, ""));
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(new Response(e.getMessage()));
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody T model) {
+        try {
+            T results = service.save(model);
+            return ResponseEntity.ok(new Response(results, "Inséré avec succes"));
+        } catch (DataIntegrityViolationException e) {
+            // e.printStackTrace();
+            System.out.println(e.getMessage());
+            if (e.getCause() instanceof ConstraintViolationException) {
+                ConstraintViolationException sqlException = (ConstraintViolationException) e.getCause();
+                String sqlState = sqlException.getSQLState();
+                if ("23505".equals(sqlState)) {
+                    String columnName = Utilities.extractColumnName(
+                            type,
+                            sqlException.getMessage());
+
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                            .body(new Response(
+                                    "Contrainte : " + columnName + " doit être unique. "));
+                }
+            }
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                    new Response("Contrainte de donnée violée"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new Response("Erreur interne du serveur"));
+        }
     }
-  }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<?> save(
-    @RequestBody T model,
-    @PathVariable(name = "id") int id
-  ) {
-    try {
-      T results = service.update(model, id);
-      return ResponseEntity.ok(new Response(results, "Modifier avec succes"));
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(new Response(e.getMessage()));
+    @GetMapping
+    public ResponseEntity<?> findAll(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "0") int pageSize) {
+
+        try {
+            if (page == 0 && pageSize == 0) {
+                List<T> results = service.findAll();
+                return ResponseEntity.ok(new Response(results, ""));
+            }
+            Paginated<T> result = service.findAll(page, pageSize);
+            return ResponseEntity.ok(new Response(result, ""));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new Response(e.getMessage()));
+        }
     }
-  }
 
-  public GenericService<T> getService() {
-    return service;
-  }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> save(
+            @RequestBody T model,
+            @PathVariable(name = "id") int id) {
 
-  public HashMap<String, Object> getRes() {
-    return res;
-  }
+        try {
+            T results = service.update(model, id);
+            return ResponseEntity.ok(new Response(results, "Modifié avec succes"));
 
-  public HashMap<String, Object> getErr() {
-    return err;
-  }
+        } catch (DataIntegrityViolationException e) {
+            // e.printStackTrace();
+            System.out.println(e.getMessage());
+            if (e.getCause() instanceof ConstraintViolationException) {
+                ConstraintViolationException sqlException = (ConstraintViolationException) e.getCause();
+                String sqlState = sqlException.getSQLState();
+                if ("23505".equals(sqlState)) {
+
+                    String columnName = Utilities.extractColumnName(type, sqlException.getMessage());
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                            .body(new Response(
+                                    "Contrainte : " + columnName + " doit être unique. "));
+                }
+            }
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                    new Response("Contrainte de donnée violée"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new Response("Erreur interne du serveur"));
+        }
+    }
+
+    public GenericService<T> getService() {
+        return service;
+    }
+
+    public HashMap<String, Object> getRes() {
+        return res;
+    }
+
+    public HashMap<String, Object> getErr() {
+        return err;
+    }
 }
---
+++ src/main/java/com/cloud/voiture/services/utilities/Utilities.java
@@ -0,0 +1,36 @@
+package com.cloud.voiture.services.utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.cloud.voiture.crud.model.GenericModel;
+
+import jakarta.persistence.Table;
+
+public class Utilities {
+
+    public static String capitalize(String word) {
+        String first = word.substring(0, 1);
+        String others = word.substring(1);
+        String result = first.toUpperCase() + others;
+        return result;
+    }
+
+    public static String extractColumnName(Class<?> T, String errorMessage) {
+        String tableName = T.getSimpleName();
+        Table table = T.getAnnotation(Table.class);
+        if (table != null) {
+            tableName = table.name();
+        }
+        System.out.println("tableName " + tableName);
+        Pattern pattern = Pattern.compile(tableName.toLowerCase() + "_(.*?)_key");
+        Matcher matcher = pattern.matcher(errorMessage);
+
+        if (matcher.find()) {
+            System.out.println("column name " + matcher.group(1));
+            return matcher.group(1);
+        }
+
+        return "";
+    }
+}
