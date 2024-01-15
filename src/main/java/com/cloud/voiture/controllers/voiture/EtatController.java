87da3b37d5f95d880cb652afceec98e9fad42406
Mialy05 <mialisoamurielle@gmail.com>
2024-01-15 16:06:33 +0300

    feat [DELETE] supprimer endpoint create et delete etat

--- src/main/java/com/cloud/voiture/controllers/voiture/EtatController.java
+++ src/main/java/com/cloud/voiture/controllers/voiture/EtatController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,24 @@ import com.cloud.voiture.services.utilities.Utilities;
 import com.cloud.voiture.services.voiture.EtatService;
 import com.cloud.voiture.types.response.Response;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/etats")
 public class EtatController extends GenericController<Etat> {
+    @Autowired
+    EtatService etatService;
+
+    @Override
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response> delete(@PathVariable(name = "id") int id) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Etat model) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
 
 }
