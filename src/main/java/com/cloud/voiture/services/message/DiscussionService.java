d7cb3b2520c4e01ceb23428a5b9b220459635534
ETU1906 <yoanrazafinjaka@gmail.com>
2024-01-28 13:21:15 +0300

    fix [DISCUSSION] : ajout exception si discussion existe déjà

--- src/main/java/com/cloud/voiture/services/message/DiscussionService.java
+++ src/main/java/com/cloud/voiture/services/message/DiscussionService.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.cloud.voiture.chat.requests.CreatePrivateChatRequest;
@@ -31,11 +33,25 @@ public class DiscussionService {
 
     public boolean allowed(String idDiscussion, int userId) {
         Discussion discussion = repository.findByIdDiscussion(idDiscussion);
+        System.out.println( userId + "  "+ discussion.getUserId1() + "  "+ discussion.getUserId2() );
+        System.out.println( discussion.getUserId1() == userId || discussion.getUserId2() == userId );
         return discussion.getUserId1() == userId || discussion.getUserId2() == userId;
     }
 
-    public Discussion save(CreatePrivateChatRequest request, int contacter) {
-
+    public Discussion save(CreatePrivateChatRequest request, int contacter) throws Exception {
+        Criteria criteria1 = Criteria.where("userId1").is(contacter).and("userId2")
+                .is(request.getTargetUserId());
+        Criteria criteria2 = Criteria.where("userId1").is(request.getTargetUserId()).and("userId2")
+                .is(contacter);
+        System.out.println( criteria1 );
+        System.out.println( criteria2 );
+        Query query = new Query(new Criteria().orOperator(criteria1, criteria2));
+        System.out.println( query );
+        List<Discussion> discussions_exist = mongoTemplate.find(query, Discussion.class);
+        System.out.println( discussions_exist );
+        if (discussions_exist.size() != 0) {
+            throw new Exception("cette discusson existe déjà");
+        }
         Discussion discussion = new Discussion();
         discussion.setUserId1(contacter);
         discussion.setUserId2(request.getTargetUserId());
