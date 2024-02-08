eaddbfa1962fa063be17572db6c719309bfb327c
Nante <nantemino15@gmail.com>
2024-02-08 17:40:16 +0300

    permit save token

--- src/main/java/com/cloud/voiture/config/SecurityConfig.java
+++ src/main/java/com/cloud/voiture/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.PUT, "/couleurs").hasAuthority("ADMIN")
                             .requestMatchers(HttpMethod.DELETE, "/couleurs").hasAuthority("ADMIN")
                             .requestMatchers("/annonces/estimate").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/notif-token").permitAll()
                             .anyRequest().authenticated();
 
                 })
